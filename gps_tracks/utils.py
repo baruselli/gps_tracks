1c0a5db36c55ff3a34f5304765cad8e811b7e711
Pier Paolo Baruselli <pierpaolo.baruselli@prodigysgroup.it>
2021-08-17 22:22:58 +0200

    fix match_url_path

--- gps_tracks/utils.py
+++ gps_tracks/utils.py
@@ -5,5 +5,9 @@ def match_url_path(path):
     """associate an url to each media path"""
     if path == settings.MEDIA_BASE_DIR:
         return settings.MEDIA_URL
+    elif settings.MEDIA_BASE_DIR in path:
+        import os
+        rel_path_name = os.path.relpath(path,settings.MEDIA_BASE_DIR).replace("\\","/")
+        return settings.MEDIA_URL + rel_path_name + "/"
     else:
         return "/media_" + path.replace("/","_").replace("\\","_").replace(":","_").replace(" ","_").replace("'","_").replace('"',"_")+"/"

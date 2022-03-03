c4310cd5059acd6859cb626dd74b0b01cebdc1ff
Pier Paolo Baruselli <pierpaolo.baruselli@gmail.com>
2022-03-03 21:55:10 +0100

    fix slideshow

--- static/js/custom/slideshow.js
+++ static/js/custom/slideshow.js
@@ -12,7 +12,13 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
+    //var captionText = document.getElementById("caption");
+    if (slides.length == 0){
+        $("#slideshow_container").hide()
+        return
+    }else{
+        $("#slideshow_container").show()
+    }
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -23,7 +29,7 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    //captionText.innerHTML = dots[slideIndex-1].alt;
 } 
 
 
@@ -45,7 +51,6 @@ $(document).keydown(function(e){
 
 $(document).ready( function () {
     if ($("#slideshow_container").length >0){
-        console.log("jjjj", $("#slideshow_container").length)
         window.slideIndex = 1
         if (("#slideshow_container").length){
             showSlides(slideIndex);
